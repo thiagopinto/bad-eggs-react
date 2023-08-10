4851339fbec25e75ad28fd59a1bd3df66f19b561
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2023-08-09 22:18:39 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,11 @@ export default defineConfig(({ mode }) => {
       } 
     },
     plugins: [react()],
+    root: "./",
+    build: {
+      outDir: "dist",
+    },
+    base: "/eggs/",
+    publicDir: "public"
   };
 });
