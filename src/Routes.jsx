595b7cc0affdb88ef5a6fd48a87ebe04d6c944fb
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2023-08-10 06:50:04 -0300

    create basename

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,4 +1,8 @@
-import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
 import { useAuthProvider } from "./contexts/AuthContext";
 import LayoutDashbord from "./layouts/LayoutDashbord";
 import NotFound from "./pages/NotFound";
@@ -11,49 +15,53 @@ import Cycle from "./pages/Ovitrampas/Cycles";
 import Image from "./pages/Ovitrampas/Cycles/Images";
 import Prediction from "./pages/Ovitrampas/Cycles/Images/Prediction";
 
-
 export default function routes() {
   let { userAuth } = useAuthProvider();
 
-  const router = createBrowserRouter([
+  const router = createBrowserRouter(
+    [
+      {
+        element: <LayoutDashbord />,
+        errorElement: <NotFound />,
+        children: [
+          {
+            path: "/",
+            element: <Home />,
+          },
+          {
+            path: "/gallery",
+            element: <Gallery />,
+          },
+          {
+            path: "/ovitrampa",
+            element: userAuth ? <Ovitrampas /> : <Navigate to="/" />,
+          },
+          {
+            path: "/ovitrampa/:ovitrampaId",
+            element: userAuth ? <Cycle /> : <Navigate to="/" />,
+          },
+          {
+            path: "/ovitrampa/:ovitrampaId/cycle/:cycleId",
+            element: userAuth ? <Image /> : <Navigate to="/" />,
+          },
+          {
+            path: "/ovitrampa/:ovitrampaId/cycle/:cycleId/prediction/:ImageId",
+            element: userAuth ? <Prediction /> : <Navigate to="/" />,
+          },
+          {
+            path: "/user",
+            element: userAuth ? <User /> : <Navigate to="/" />,
+          },
+          {
+            path: "/login",
+            element: !userAuth ? <Login /> : <Navigate to="/" />,
+          },
+        ],
+      },
+    ],
     {
-      element: <LayoutDashbord />,
-      errorElement: <NotFound />,
-      children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
-        {
-          path: "/gallery",
-          element: <Gallery />,
-        },
-        {
-          path: "/ovitrampa",
-          element: userAuth ? <Ovitrampas /> : <Navigate to="/" />,
-        },
-        {
-          path: "/ovitrampa/:ovitrampaId",
-          element: userAuth ? <Cycle /> : <Navigate to="/" />,
-        },
-        {
-          path: "/ovitrampa/:ovitrampaId/cycle/:cycleId",
-          element: userAuth ? <Image /> : <Navigate to="/" />,
-        },
-        {
-          path: "/ovitrampa/:ovitrampaId/cycle/:cycleId/prediction/:ImageId",
-          element: userAuth ? <Prediction /> : <Navigate to="/" />,
-        },
-        {
-          path: "/user",
-          element: userAuth ? <User /> : <Navigate to="/" />,
-        },
-        {
-          path: "/login",
-          element: !userAuth ? <Login /> : <Navigate to="/" />,
-        },
-      ],
-    },
-  ]);
+      basename: process.env.BASE_NAME,
+    }
+  );
   return <RouterProvider router={router} />;
 }
