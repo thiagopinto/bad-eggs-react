21d60c6a05a224e9ba299f284f7057c4120501bc
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2023-08-10 07:59:16 -0300

    create basename

--- src/layouts/LayoutDashbord.jsx
+++ src/layouts/LayoutDashbord.jsx
@@ -58,7 +58,7 @@ export default function Dashboard() {
               <HeaderName as={Link} to="/" prefix="">
                 <img
                   className="cds--aspect-ratio cds--aspect-ratio--1x1 image-logo"
-                  src="bad_egg_mini.jpg"
+                  src="bad_egg_mini.png"
                   alt="logo"
                 />
                 BadEggs<small>[Zoonoses]</small>
